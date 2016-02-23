a862aaa78d7959b8888ca0a5647982165c5a26da
Joseph Nolan <josephbnolan@me.com>
2016-02-23 16:50:43 -0600

    Finished quotebook project

---
+++ js/mainCtrl.js
@@ -0,0 +1,27 @@
+angular.module('quoteBook')
+  .controller('mainCtrl', ['$scope', 'dataService', function($scope, dataService) {
+
+
+        $scope.quoteData = dataService.getData();
+        $scope.filterType = "";
+
+        $scope.addData = function(data) {
+          var obj = { text: $scope.quoteInput, author: 'Anonymous' };
+          dataService.addData(obj);
+          $scope.quoteInput = '';
+        }
+
+        $scope.removeData = function(data) {
+          dataService.removeData(data);
+          $scope.quoteInput = '';
+        };
+
+        $scope.changeFilter = function() {
+          if ($scope.filterType === "") {
+            $scope.filterType = $scope.quoteInput
+          } else {
+            $scope.filterType = "";
+          }
+        };
+
+  }]);
